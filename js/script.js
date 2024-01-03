aa14dfd23f8956a22d1ef93bef092aabb641ab3d
MdAnikMollah <mdanik0178@gmail.com>
2024-01-03 20:00:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let darkBtn = document.querySelector(".dark__btn");
+
+    darkBtn.addEventListener('click', function(){
+    darkBtn.classList.toggle("active")
+    document.body.classList.toggle('dark')
+});
